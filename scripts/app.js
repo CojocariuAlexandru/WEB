61951a9c09c865a023a694df9def5eb350e77ae5
Alexandru Oloieri <alexoloieri2014@gmail.com>
2020-03-10 23:19:33 +0200

    fixed scroll in attack/:id component

--- scripts/app.js
+++ scripts/app.js
@@ -211,6 +211,7 @@ root.children.push(mapRoute);
 let thatAttackRoute = new route(':id');
 thatAttackRoute.processCallback = function (arg) {
     this.beforeCallback(arg);
+    this.afterCallback();
 }
 thatAttackRoute.beforeCallback = function (id) {
     let found = false;
@@ -249,8 +250,7 @@ thatAttackRoute.templateCallback = function (attack) {
                         </ul>
                     </div>
                     
-                    <div class='mapAttack'>
-                        <img src='/img/worldMap.png' alt='WorldMap'>
+                    <div id='mapAttack'>
                     </div>
                     
                     <div class='attackDetailTitle1'>
@@ -284,12 +284,19 @@ thatAttackRoute.templateCallback = function (attack) {
                          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
                          </p>
                      </div>
-
-                     <footer class='byKaggle'>
-                        <p> powered by Kaggle </p>
-                     </footer>
                 </div>
             `;
+}
+
+thatAttackRoute.afterCallback = function() {
+    let attackAttack = document.querySelector('#mapAttack');
+    map = new google.maps.Map(attackAttack, {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
+    });
 }
 
 let attacksRoute = new route('attacks');
