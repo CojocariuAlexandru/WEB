0255cde959a4c018ac80ff7fc8ed54bf269a5b0b
CojocariuAlexandru <53466892+CojocariuAlexandru@users.noreply.github.com>
2020-05-19 21:07:00 +0300

    added svg beginning

--- web-app/scripts/app-statistics-result.js
+++ web-app/scripts/app-statistics-result.js
@@ -708,3 +708,29 @@ function downloadCsv(name) {
 
     link.click();
 }
+
+function downloadSVG(){
+    let piechart = document.querySelector('svg:first-child');
+    var serializer = new XMLSerializer();
+    var source = serializer.serializeToString(piechart);
+
+    if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+        source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+    }
+    if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+        source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+    }
+    source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+    var svgContent = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+
+    var encodedUri = encodeURI(svgContent);
+    window.open(encodedUri);
+    var encodedUri = encodeURI(svgContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "attacksSVG.svg");
+    document.body.appendChild(link); // Required for FF
+
+    link.click();
+}
